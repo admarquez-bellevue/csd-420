c6e09354fe85d4f0600f252c721d6ed93a1d1ef4
Adrian Marquez <adrianmarquez@Adrians-MacBook-Air.local>
2026-04-05 22:29:58 -0500

    Pushing 04/05/2026

---
+++ module-2/randomdatareader.java
@@ -0,0 +1,21 @@
+// Adrian Marquez
+// 04/05/2026
+// Module 2
+
+import java.io.*;
+import java.util.Scanner;
+
+public class randomdatareader {
+    public static void main(String[] args) {
+        String fileName = "adrian_datafile.dat";
+        
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("The file does not exist.");
+            e.printStackTrace();
+        }
+    }
+}
